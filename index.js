5cd83f4b03d01b79c74767f5eaf0fe2313678b9f
programmarimran <programmarimran@gmail.com>
2025-05-21 22:29:07 +0600

    added a Put api is created successfully

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-require('dotenv').config()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-const express = require('express')
-const app = express()
-const cors=require("cors")
-const port =process.env.port||3000
-app.use(express.json())
-app.use(cors())
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.port || 3000;
+app.use(express.json());
+app.use(cors());
 //MongoDB is start
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rdhp12d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -16,44 +16,54 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-   
     // Connect to the "sample_mflix" database and access its "movies" collection
     const database = client.db("diverseDishDB");
     const productCollection = database.collection("recipes");
-    const userCollection=database.collection("users")
+    const userCollection = database.collection("users");
 
-    app.get("/recipes",async(req,res)=>{
-        const cursor=productCollection.find()
-        const result=await cursor.toArray()
-        res.send(result)
-    })
-    app.get("/recipes/:id",async(req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
-      const result=await productCollection.findOne(query)
+    app.get("/recipes", async (req, res) => {
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/recipes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.findOne(query);
+      res.send(result);
+    });
+    app.post("/recipes", async (req, res) => {
+      const newProducts = req.body;
+      const result = await productCollection.insertOne(newProducts);
+      res.send(result);
+    });
+    app.put("/recipes/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set:req.body
+      };
+      const result=await productCollection.updateOne(filter,updateDoc,options)
       res.send(result)
+    });
+    app.delete("/recipes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await productCollection.deleteOne(query);
+      res.send(result);
+    });
 
-    })
-    app.post("/recipes",async(req,res)=>{
-        const newProducts=req.body;
-        const result= await productCollection.insertOne(newProducts)
-        res.send(result)
-    })
-    app.delete("/recipes/:id",async(req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
-      const result=await productCollection.deleteOne(query)
-      res.send(result)
-    })
-    
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -61,13 +71,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-  res.send('Diverse Dish Server is Running>......')
-})
-
+app.get("/", (req, res) => {
+  res.send("Diverse Dish Server is Running>......");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
